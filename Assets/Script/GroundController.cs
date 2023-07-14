c7e97722e3ae116601d5b3baa392110b8d136f7e
LambFerret <dignrjrl12@gmail.com>
2023-07-14 19:36:48 +0900

    horse animation

--- Assets/Script/GroundController.cs
+++ Assets/Script/GroundController.cs
@@ -7,7 +7,9 @@ namespace Script
     public class GroundController : MonoBehaviour
     {
         public List<GameObject> groundPrefabs;
+        public List<GameObject> foregrounds;
         public PlayerController player;
+        public float foregroundSpeedMultiplier = 3F;
 
         private float _groundWidth;
         private float _totalGroundWidth;
@@ -20,31 +22,47 @@ namespace Script
             {
                 GameObject ground = Instantiate(groundInstance, transform);
                 ground.transform.position = new Vector3(_totalGroundWidth, 0, 0);
+                // 여기서 array 안의 토탈 길이를 계산
                 _totalGroundWidth += ground.transform.Find("Ground").GetComponent<SpriteRenderer>().bounds.size.x;
                 _groundInstances.Add(ground);
             }
 
+            // 첫번째 array에 있는 element 의 길이를 저장
             _groundWidth = _groundInstances[0].transform.Find("Ground").GetComponent<SpriteRenderer>().bounds.size.x;
+
+            foreach (GameObject foreground in foregrounds)
+            {
+                Sequence sequence = DOTween.Sequence();
+                sequence.Append(foreground.transform.DOMoveX(-_groundWidth, player.speed / foregroundSpeedMultiplier).SetEase(Ease.Linear));
+                sequence.Append(foreground.transform.DOMoveX(foreground.transform.position.x, 0));
+                sequence.SetLoops(-1);
+
+            }
         }
 
         private void Update()
         {
+            // 무시
             foreach (GameObject ground in _groundInstances)
             {
                 ground.transform.position -= new Vector3(player.speed * Time.deltaTime, 0, 0);
             }
 
+            // 만일 array에서 첫번째 element의 x 좌표가 -_groundWidth 보다 작거나 같다면
             if (_groundInstances[0].transform.position.x <= -_groundWidth)
             {
+                // 첫번째 element의 좌표를 토탈 길이를 참조하여 변경
                 _groundInstances[0].transform.position = new Vector3
                 (
                     _totalGroundWidth - _groundInstances[0].transform.position.x,
                     _groundInstances[0].transform.position.y,
                     _groundInstances[0].transform.position.z
                 );
+                // 첫번째 element를 array의 마지막 element로 옮김
                 GameObject obj = _groundInstances[0];
                 _groundInstances.RemoveAt(0);
                 _groundInstances.Add(obj);
+                // 0번째 element 길이 재저장
                 _groundWidth = _groundInstances[0].transform.Find("Ground").GetComponent<SpriteRenderer>().bounds.size.x;
             }
         }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,29 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Script
 {
     public class PlayerController : MonoBehaviour
     {
-
         public float jumpForce = 5f;
         public int life = 10;
         public float invincibleTime = 2f;
         public int score;
         public int speed;
+        public GameObject playerPrefab;
 
         private List<IPlayerObserver> _observers = new List<IPlayerObserver>();
+        private List<GameObject> players = new List<GameObject>();
         private bool _isJumping;
         private bool _isInvincible;
         private Rigidbody2D _rb;
         private SpriteRenderer _sprite;
+        private GridLayoutGroup _lifeGroup;
 
         private void Start()
         {
+            _lifeGroup = transform.Find("LifeContainer").GetComponent<GridLayoutGroup>();
             _rb = GetComponent<Rigidbody2D>();
             _sprite = GetComponent<SpriteRenderer>();
+            for (int i = 0; i < life; i++)
+            {
+                AddLifeIntoGrid();
+            }
         }
 
         public void Jump()
@@ -45,6 +54,7 @@ namespace Script
                 _sprite.enabled = true;
                 yield return new WaitForSeconds(0.1f);
             }
+
             _isInvincible = false;
         }
 
@@ -93,6 +103,7 @@ namespace Script
         {
             life += amount;
             NotifyLifeChanged(amount);
+            AddLifeIntoGrid();
         }
 
         public void DecreaseLife(int damage)
@@ -105,7 +116,28 @@ namespace Script
                 Debug.Log("Player is dead");
             }
 
+            RemoveLifeFromGrid();
             StartCoroutine(Blink());
+        }
+
+        private void AddLifeIntoGrid()
+        {
+            GameObject newPlayer = Instantiate(playerPrefab, _lifeGroup.transform);
+            players.Add(newPlayer);
+        }
+
+        private void RemoveLifeFromGrid()
+        {
+            if (players.Count > 0)
+            {
+                GameObject lastPlayer = players[^1];
+                players.RemoveAt(players.Count - 1);
+                Destroy(lastPlayer);
+            }
+            else
+            {
+                Debug.Log("No players to remove");
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
